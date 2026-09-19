6fb408279f75c61c3b5aa411206d0caa6e79f8d1
kimkhadija777 <kimkhadija590@gmail.com>
2026-09-19 18:16:38 +0500

    Create rag_engine.py

---
+++ rag_engine.py
@@ -0,0 +1,19 @@
+rag_engine.py
+
+        ↓
+
+Load FAISS
+        ↓
+Load metadata
+        ↓
+Load embedding model
+        ↓
+Receive question
+        ↓
+Semantic search
+        ↓
+Retrieve relevant chunks
+        ↓
+Send context to Groq
+        ↓
+Return answer
